8c55712e27f50b499e04a30b7a0685b8706ff71d
Kevin Li <kevinli29033@gmail.com>
2020-06-01 08:35:32 -0700

    Initial Commit

---
+++ TrackableList/ContentView.swift
@@ -0,0 +1,15 @@
+// Kevin Li - 8:35 AM - 6/1/20
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
